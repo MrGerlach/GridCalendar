47786edabe831aaa9b4e7c6c1238c2077a6a2f09
Mr. Gerlach <mgerl@tlen.pl>
2022-07-24 14:21:35 +0200

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Michał Gerlach on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
